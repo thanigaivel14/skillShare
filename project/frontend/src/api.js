6b878b3c66ca10e1d394a8600941655ef1464708
Thanigaivel <119962410+thanigaivel14@users.noreply.github.com>
2025-07-30 13:55:42 +0530

    Update api.js

--- project/frontend/src/api.js
+++ project/frontend/src/api.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://skillshare-1j86.onrender.com/api", // adjust if different
-  
-  withCredentials: true,                // send HTTP-only cookies
+  baseURL: import.meta.env.VITE_API_BASE_URL + "/api",
+  withCredentials: true,
 });
 
 export default API;
