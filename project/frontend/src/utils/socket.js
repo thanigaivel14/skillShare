25daf58a21f2f0ecf97bac028fa18fe1e602173e
Thanigaivel <119962410+thanigaivel14@users.noreply.github.com>
2025-07-30 11:16:39 +0530

    Update socket.js

--- project/frontend/src/utils/socket.js
+++ project/frontend/src/utils/socket.js
@@ -1,7 +1,7 @@
 // src/socket.js
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:8000", {
+const socket = io("https://skillshare-1j86.onrender.com", {
     transports: ["websocket"],
   withCredentials: true,
 });
