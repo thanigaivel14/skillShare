0d057a6834e856c64f4391d1dfe60e8a1a8c8595
Thanigaivel <119962410+thanigaivel14@users.noreply.github.com>
2025-07-30 11:10:14 +0530

    Update server.js

--- project/backend/server.js
+++ project/backend/server.js
@@ -7,7 +7,7 @@ const server=http.createServer(app);
 
 const io= new Server(server,{
     cors:{
-        origin:"http://localhost:5173",
+        origin:"https://skillshare-1-pmeq.onrender.com",
         methods:["GET,POST"],
         transports: ["websocket"],
         Credential:true
