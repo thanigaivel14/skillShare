59ac9f77686d60512494536dcef0dddcf0ab58ba
thanigaivel14 <thanigai905@gmail.com>
2025-08-03 19:55:31 +0530

    update

--- project/frontend/src/utils/socket.js
+++ project/frontend/src/utils/socket.js
@@ -1,9 +1,13 @@
 // src/socket.js
 import { io } from "socket.io-client";
 
-const socket = io("import.meta.env.VITE_SOCKET_URL", {
-    transports: ["websocket"],
-  withCredentials: true,
-});
+const socket = io(
+  (import.meta.env.PROD ? `wss://${import.meta.env.VITE_SOCKET_URL}` : `ws://${import.meta.env.VITE_SOCKET_URL}`),
+  {
+    transports: ['websocket'],
+    withCredentials: true,
+  }
+);
+
 
 export default socket;
