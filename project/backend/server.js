bdfb386c27a5899794c30d7f6409e9d7ca666cf2
Thanigaivel <119962410+thanigaivel14@users.noreply.github.com>
2025-08-05 19:19:11 +0530

    Update server.js (reload )

--- project/backend/server.js
+++ project/backend/server.js
@@ -10,7 +10,7 @@ const __dirname1 = path.resolve("../");
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "frontend", "dist")));
 
-  app.get("/", (req, res) => {
+  app.get(/(.)/, (req, res) => {
     res.sendFile(path.join(__dirname1, "frontend", "dist", "index.html"));
   });
 }
